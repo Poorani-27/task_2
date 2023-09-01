26b5dc345e040876494373a9375a7c3d367a2a79
POORANI TAMILVASAN <125793555+Poorani-27@users.noreply.github.com>
2023-09-01 21:44:08 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,11 @@
+a= float(input("Enter the value of a :"))
+b= float(input("Enter the value of b:"))
+#floor Division
+print("Floor Division:" ,int(a//b))
+#a power b
+print("a power b:" , int(a**b))
+#Equal values
+if (a==b):
+    print("Equal values:Ture")
+else:
+    print("Equal values:False")
